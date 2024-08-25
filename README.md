26ed5675dadaa48273e975af345350a56a25765e
David pequeno Marinho <63377354+David-Marinho@users.noreply.github.com>
2024-08-25 17:10:25 -0300

    .
    .

--- README.md
+++
@@ -1,2 +0,0 @@
-# dg-assossoria-site
- 
--- main.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {
-    menu = document.getElementById("menu")
-    if (menu.style.display == "none") {
-        menu.style.display = "block"
-    }
-    else {
-       menu.style.display = "none" 
-    }
-}
